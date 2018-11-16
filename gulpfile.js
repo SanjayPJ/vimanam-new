54121ea3974c12a29d90b640a2af42475baac110
sanjaypj <sanjaypjayan2000@gmail.com>
2018-11-16 22:28:13 +0530

    base set up

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('compile-scss', function () {
+    gulp.watch('assets/scss/*.scss', function () {
+        console.log("compiling scss...");
+        return gulp.src('assets/scss/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('assets/css'));
+    });
+});
